55125cd6508c8d54488b1a83a0a8a1879e3a0e62
Saksham Mittal <gotlouemail@gmail.com>
2022-05-10 22:06:42 +0530

    Clean up references, remove citation links etc
    This is to make the output look much more presentable. Links to files
    have also been fixed thanks to addition of more characters in the regex
    match.
    
    For parsing the HTML page, Beautiful Soup has been used. The
    requirements.txt has been appropriately updated

--- crawler/cleanse.py
+++ crawler/cleanse.py
@@ -1,17 +1,38 @@
 import re
+from bs4 import BeautifulSoup
+
+def decomposeClasses(element, classList, soup):
+
+    for classname in classList:
+        for e in soup.find_all(element, {"class": classname}):
+            e.decompose()
 
 with open("table.json") as f:
     
     x = f.readlines()
+    s = 0
     for i in x:
         i = i[1:-2]
         i = i.replace("\\n","")
         #regex substitution
         try:
-            results = re.findall(r'href=\\"[A-Za-z0-9&?=_:;/]*\\"', i)
+            results = re.findall(r'href=\\"[A-Za-z0-9&?=$#%.-_:/;]*\\"', i)
+            #results = re.findall(r'href=\\"[A-Za-z0-9&?=-_:;/]*\\"', i)
             for r in results:
                 newr =  'href="https://en.wikipedia.org' + r[7:-2] + '"'
                 i = i.replace(r, newr)
-            print(i)
+
+            soup = BeautifulSoup(i, features="html.parser")
+            
+            divClasses = ['\\"mw-references-wrap\\"', '\\"mw-references-wrap', '\\"mw-editsection\\"']
+            spanClasses = ['\\"mw-editsection\\"']
+            supClasses = ['\\"reference\\"']
+        
+            decomposeClasses("div", divClasses, soup)
+            decomposeClasses("span", spanClasses, soup)
+            decomposeClasses("sup", supClasses, soup)
+
+            print(soup)
+
         except:
             continue
