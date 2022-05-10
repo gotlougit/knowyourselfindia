b7c9f235373c9acfcda7274e39848cc8c0c17400
Saksham Mittal <gotlouemail@gmail.com>
2022-05-10 22:09:44 +0530

    Remove old regex and testing variables in cleanse.py

--- crawler/cleanse.py
+++ crawler/cleanse.py
@@ -10,14 +10,12 @@ def decomposeClasses(element, classList, soup):
 with open("table.json") as f:
     
     x = f.readlines()
-    s = 0
     for i in x:
         i = i[1:-2]
         i = i.replace("\\n","")
         #regex substitution
         try:
             results = re.findall(r'href=\\"[A-Za-z0-9&?=$#%.-_:/;]*\\"', i)
-            #results = re.findall(r'href=\\"[A-Za-z0-9&?=-_:;/]*\\"', i)
             for r in results:
                 newr =  'href="https://en.wikipedia.org' + r[7:-2] + '"'
                 i = i.replace(r, newr)
