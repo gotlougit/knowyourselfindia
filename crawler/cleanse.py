3991d262c1a3f42d6b0bf8e75f17545641146642
Saksham Mittal <gotlouemail@gmail.com>
2022-05-13 10:18:32 +0530

    Encode Unicode characters properly from Wikipedia API output

--- crawler/cleanse.py
+++ crawler/cleanse.py
@@ -13,13 +13,14 @@ with open("table.json") as f:
     for i in x:
         i = i[1:-2]
         i = i.replace("\\n","")
+        i = i.encode().decode("unicode-escape")
         #regex substitution
         try:
             results = re.findall(r'href=\\"[A-Za-z0-9&?=$#%.-_:/;]*\\"', i)
             for r in results:
                 newr =  'href="https://en.wikipedia.org' + r[7:-2] + '"'
                 i = i.replace(r, newr)
-
+            
             soup = BeautifulSoup(i, features="html.parser")
             
             divClasses = ['\\"mw-references-wrap\\"', '\\"mw-references-wrap', '\\"mw-editsection\\"']
